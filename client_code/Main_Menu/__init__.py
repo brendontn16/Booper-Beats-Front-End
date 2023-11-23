ea902546cc0a8681673e1a223bdcfa3d9c8c9818
secretasian16@gmail.com <secretasian16@gmail.com>
2023-11-23 01:13:01 +0000

    Created Main_Menu.Form2, Main_Menu and LICENSE.txt; Edited settings and theme/parameters.yaml

---
+++ client_code/Main_Menu/__init__.py
@@ -0,0 +1,13 @@
+from ._anvil_designer import Main_MenuTemplate
+from anvil import *
+
+class Main_Menu(Main_MenuTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def button_4_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
