ca53486e650cb2cdc6d2f7ba5e78b6f6ac1d96d8
rfernan7@kent.edu <rfernan7@kent.edu>
2023-12-01 02:04:48 +0000

    Edited Main_Menu.MusicPlayer

--- client_code/Main_Menu/MusicPlayer/__init__.py
+++ client_code/Main_Menu/MusicPlayer/__init__.py
@@ -13,6 +13,20 @@ class MusicPlayer(MusicPlayerTemplate):
     open_form('Main_Menu.ORDER_NOW.Create_A_Pizza')
     pass
 
+  def slider_1_change(self, **event_args):
+    self.label_4.text = self.slider_1.level
+
+  def slider_2_change(self, **event_args):
+    """This method is called when the slider is moved"""
+    self.label_3.text = self.slider_2.level
+
+  def reset_btn_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.slider_1.level = 1
+    self.slider_2.level = 1
+    self.label_4.text = 1
+    self.label_3.text = 1
+
 
 
  
