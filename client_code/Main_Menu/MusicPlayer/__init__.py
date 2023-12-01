39268466e9ac7dd5907c0eba5f481bf9f2252797
cup00joe@gmail.com <cup00joe@gmail.com>
2023-12-01 02:06:28 +0000

    Edited Main_Menu.MusicPlayer

--- client_code/Main_Menu/MusicPlayer/__init__.py
+++ client_code/Main_Menu/MusicPlayer/__init__.py
@@ -13,9 +13,10 @@ class MusicPlayer(MusicPlayerTemplate):
     open_form('Main_Menu.ORDER_NOW.Create_A_Pizza')
     pass
 
-  def button_11_click(self, **event_args):
+  def back_click(self, **event_args):
     """This method is called when the button is clicked"""
     open_form('Main_Menu')
     pass
+
 
  
