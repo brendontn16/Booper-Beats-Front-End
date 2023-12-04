3cb9f19d306ec85353d8a2f273ceced814c7400b
ccoggins@kent.edu <ccoggins@kent.edu>
2023-12-04 11:09:45 +0000

    Edited Main_Menu.ORDER_NOW.Beverages and settings

--- client_code/Main_Menu/ORDER_NOW/Beverages/__init__.py
+++ client_code/Main_Menu/ORDER_NOW/Beverages/__init__.py
@@ -10,6 +10,28 @@ class Beverages(BeveragesTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    # ensure page data is clear before generation
+    self.card_1.clear()
+    # Make some default columns to be populated within the card
+    drink_columns = 4
+
+    #make an array of panels
+    drink_column_panels = [ColumnPanel() for _ in range(drink_columns)]
+    #add those components to the 'card_1' column panel
+    for drink_column_panel in drink_column_panels:
+      self.card_1.add_component(drink_column_panel)
+    
+    #get a list of drinks from the fullmenu
+    drinks_list = app_tables.fullmenu.search(Menu_Type='Drinks')
+    buttons_per_column = len(drinks_list) // drink_columns
+
+    #generate a button for each of these drinks into the columned panels
+    for i, row in enumerate(drinks_list):
+      drink_button = Button(text=row['Specific_Item'])
+      #give the button interactivity
+      drink_column_index = i % buttons_per_column
+      drink_column_panels[drink_column_index].add_component(drink_button)
+      
 
   def button_11_click(self, **event_args):
     """This method is called when the button is clicked"""
