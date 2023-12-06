65f377acb636fb86123ed34d505a901818530668
usr/bin/poobinus <129819902+FernsReuben@users.noreply.github.com>
2023-12-05 22:41:46 -0500

    MUSIC_BOOPERBEATS

---
+++ MUSIC_SECTION_BOOPERBEATS/app.py
@@ -0,0 +1,99 @@
+"""
+Written By: Reuben Fernandes
+Date: 11/25/2023
+Description: A music player using the Spotify API. 
+⠀⠀⠀⠀⢀⣤⡀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣿⠉⢻⠟⢹⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⢀⣿⡄⠀⠀⣼⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣄⣠⣤⣄⠀⠀⠀⠀
+⠀⠀⣰⡿⠋⠀⣀⣀⠈⣿⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣇⠘⠋⠀⣿⠇⠀⠀⠀
+⠀⣠⡟⠀⢀⣾⠟⠻⠿⠿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⡀⠀⠀⣾⠋⢀⣀⠈⠻⢶⣄⠀⠀
+⢠⣿⠁⣰⡿⠁⠀⣀⣤⣶⣶⡶⢶⣤⣄⡀⢀⣠⠴⠚⠉⠉⠉⠉⠉⠙⢶⡄⠛⠒⠛⠙⢳⣦⡀⠹⣆⠀
+⢸⡇⢠⣿⣠⣴⣿⡟⢉⣠⠤⠶⠶⠾⠯⣿⣿⣧⣀⣤⣶⣾⣿⡿⠿⠛⠋⢙⣛⡛⠳⣄⡀⠙⣷⡀⢹⡆
+⢸⠀⢸⣿⣿⣿⣿⠞⠉⠀⠀⠀⠀⣀⣤⣤⠬⠉⠛⠻⠿⠟⠉⢀⣠⢞⣭⣤⣤⣍⠙⠺⢷⡀⢸⡇⠀⣿
+⢸⠀⢸⣿⣿⡟⠀⠀⠀⢀⣠⠞⣫⢗⣫⢽⣶⣤⣀⠉⠛⣶⠖⠛⠀⣾⡷⣾⠋⣻⡆⠀⠀⡇⣼⠇⠀⣿
+⢸⠀⠀⣿⣿⡇⢠⡤⠔⣋⡤⠞⠁⢸⣷⣾⣯⣹⣿⡆⢀⣏⠀⠈⠈⣿⣷⣼⣿⠿⠷⣴⡞⠀⣿⠀⠀⣿
+⢸⠀⠀⢿⣿⡇⠀⠀⠘⠻⠤⣀⡀⠸⣿⣯⣿⣿⡿⠷⠚⠉⠛⠛⠛⠛⠉⠉⠀⣠⡾⠛⣦⢸⡏⠀⠀⣿
+⢸⠀⠀⢸⣿⡇⠀⣠⠶⠶⠶⠶⠿⣿⣭⣭⣁⣀⣠⣤⣤⣤⣤⣤⣤⡶⠶⠛⠋⢁⣀⣴⠟⣽⠇⠀⠀⣿
+⢸⠀⠀⢸⣿⡇⢾⣅⠀⠀⠶⠶⢦⣤⣤⣀⣉⣉⣉⣉⣁⣡⣤⣤⣴⡶⠶⠶⠚⠉⢉⡿⣠⠟⠀⠀⣰⡟
+⢸⡀⠀⠀⢿⣇⠀⠈⠛⠳⠶⠤⠤⢤⣀⣉⣉⣉⣉⣉⣉⣁⣀⣠⣤⡤⠤⠤⠶⠞⢻⡟⠃⠀⠀⣰⠟⠀
+⢸⣧⠀⠀⠘⣿⣦⣄⡀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⣠⣤⣶⣿⣧⣀⣴⠟⠃⠀⠀
+⠀⢻⣆⠀⠀⠈⢻⣿⣿⣷⣶⣤⣄⣀⣀⣀⣠⣤⣶⣶⣶⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣟⡉⠀⠀⠀⠀⠀
+⠀⠀⢻⣦⡄⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀
+⠀⢀⣿⣿⣿⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡧⠀⠀⠀⠀
+"""
+
+from flask import Flask, render_template, jsonify  
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotify import play_preselected_playlist, control_playback, search_and_add_to_playlist, preselected_playlists, get_album_artwork  # Import get_album_artwork
+
+app = Flask(__name__)
+
+# Spotify API credentials
+CLIENT_ID = "0d0fa1cb002742e2af55ffc57265278f"
+CLIENT_SECRET = "86588df1a2924918b37a6ab61dd72002"
+REDIRECT_URI = "http://localhost:8888/callback"
+
+# Spotify API scope
+SCOPE = "user-library-read user-read-playback-state user-modify-playback-state playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public"
+
+# Initialize Spotipy with authentication
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI, scope=SCOPE))
+
+preselected_playlists = [
+    "spotify:playlist:7Gk8MHzbGL1dyrEpCM6jgB",
+    "spotify:playlist:3P2XUd8YlIQYCA6rECPGeN",
+    "spotify:playlist:1zOEqfxkCekU1Y6nZoeuyI",
+    # Add more pre-selected playlist URIs as needed
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', playlist_count=len(preselected_playlists))
+
+# Other routes and functions can be added here...
+
+@app.route('/play_playlist/<int:playlist_number>')
+def play_playlist(playlist_number):
+    if 1 <= playlist_number <= len(preselected_playlists):
+        playlist_uri = preselected_playlists[playlist_number - 1]
+        play_preselected_playlist(playlist_uri, shuffle=True)
+        return f"Playback started for playlist {playlist_number}!"
+    else:
+        return "Invalid playlist number."
+
+@app.route('/control_playback/<action>')
+def control(action):
+    control_playback(action)
+    return f"Playback controlled: {action}"
+
+@app.route('/search_and_add_to_playlist/<int:playlist_number>')
+def search_and_add(playlist_number):
+    if 1 <= playlist_number <= len(preselected_playlists):
+        playlist_uri = preselected_playlists[playlist_number - 1]
+        search_and_add_to_playlist(playlist_uri)
+        return f"Search and add to playlist {playlist_number} completed!"
+    else:
+        return "Invalid playlist number."
+    
+@app.route('/get_information')
+def get_information():
+    current_playlist, current_track = play_preselected_playlist('', shuffle=False)
+
+    if current_playlist is not None and current_track is not None:
+        data = {
+            'current_playlist': current_playlist,
+            'current_track': f'{current_track["name"]} by {", ".join([artist["name"] for artist in current_track["artists"]])}',
+            'current_artwork': get_album_artwork(current_track["uri"]),  # Use get_album_artwork from spotify.py
+        }
+    else:
+        data = {
+            'current_playlist': None,
+            'current_track': None,
+            'current_artwork': None,
+        }
+
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
